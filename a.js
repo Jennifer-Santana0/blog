3c7ee69ed81da636fdff8f726ef176facc222459
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-02-20 20:26:49 -0300

    Adicionei validacao nos form.

---
+++ a.js
@@ -0,0 +1,4 @@
+let l = []
+l.push({nome:'h'})
+l.push({nome:'a'})
+console.log(l[0].nome)
--- routes/admin.js
+++ routes/admin.js
@@ -16,20 +16,38 @@ router.get('/admin',(req,res)=>{
 router.get('/admin/categorias',(req,res)=>{
     res.render('admin/categorias')
 })
+
 router.get('/admin/categorias/add',(req,res)=>{
-    res.render('admin/addcategorias')
+    res.render('admin/addcategorias',{erros:[]})
 })
 router.post('/admin/categorias/nova',(req,res)=>{
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+
+    let erros = []
+
+    if(req.body.nome==''){
+        erros.push({texto:'O campo nome esta vazio'})
+    }
+    if(req.body.slug==''){
+        erros.push({texto:'O campo nome esta vazio'})
     }
 
-    new Categoria(novaCategoria).save().then(()=>{
-        console.log('categoria salva')
-    }).catch((err)=>{
-        console.log('erro ao salvar categoria')
-    })
+    if (erros.length>0){
+        res.render('admin/addcategorias', {erros})
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(novaCategoria).save().then(()=>{
+            console.log('categoria salva')
+        }).catch((err)=>{
+            console.log('erro ao salvar categoria ' + err)
+        })
+    }
+
+    
 })
 
 module.exports = router
+

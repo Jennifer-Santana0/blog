3865702bc599919e94618005982504858efb3396
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-02-20 11:13:03 -0300

    Fiz a pagina principal da admin e criei uma pasta partials para poder reutilizar o codigo ejs.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//INICIALIZANDO UM PROJETO DE CADASTRO
+// CARREGANDO MODULOS
+    const express = require('express')
+    const path = require('path')
+    const admin = require('./routes/admin')
+    const app = express()
+
+//CONFIGURACOES
+    app.set('view engine', 'ejs')
+    app.use(express.static(path.join(__dirname, 'public')))
+    app.use(express.urlencoded({ extended: true }));
+
+
+// ROTAS
+    app.use('/',admin)
+
+
+// OUTROS
+const PORT = 8081
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`)
+})

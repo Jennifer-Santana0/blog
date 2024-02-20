488f4df440243368c57e59e46e7f5d7ca498ac4f
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-02-20 15:17:44 -0300

    instalei o session e flash.

--- app.js
+++ app.js
@@ -3,11 +3,34 @@
     const path = require('path')
     const admin = require('./routes/admin')
     const mongoose = require('mongoose')
+    const session = require('express-session')
+    const flash = require('connect-flash')
     const app = express()
 
 //CONFIGURACOES
+    //sessao
+    app.use(session({
+        secret: 'nodejs',
+        resave: true,
+        saveUninitialized: true
+    }))
+    //flash
+    app.use(flash())
+
+    //middlewares
+    app.use((req,res,next)=>{
+        res.locals.success_msg = req.flash('success_msg')
+        res.locals.error_msg = req.flash('error_msg')
+        next()
+    })
+
+    // utilizando ejs
     app.set('view engine', 'ejs')
+
+    // utilizando css e outros
     app.use(express.static(path.join(__dirname, 'public')))
+
+    // utilizando req.body
     app.use(express.urlencoded({ extended: true }));
 
     // mongoose
@@ -18,6 +41,13 @@
     }).catch((err)=>{
         console.log('erro ao se conectar ao banco de dados ' + err)
     })
+
+    //utilizando middlewares
+    app.use((req,res,next)=>{
+        console.log('ola mundo, middlewares')
+        next()
+    }) 
+
 
 // ROTAS
     app.use('/',admin)

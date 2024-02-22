accbcde717d0ae76fd8996ce6912febeb599de2b
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-02-22 14:00:07 -0300

    Adicionei a funcao de editar o post e deletar.

--- routes/admin.js
+++ routes/admin.js
@@ -54,8 +54,42 @@ router.post('/admin/categorias/nova',(req,res)=>{
 })
 
 router.get('/admin/categorias/edit/:id',(req,res)=>{
-    res.render('admin/editcategorias')
+    Categoria.findOne({_id:req.params.id}).then((categoria)=>{
+        res.render('admin/editcategorias', {categoria})
+    }).catch((err)=>{
+        req.flash('error_msg','Esta categoria nao existe')
+        res.redirect('/admin/categorias')
+    })
 })
+
+router.post('/admin/categorias/edit',(req,res)=>{
+    Categoria.findOne({_id: req.body.id}).then((categoria)=>{
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(()=>{
+            req.flash('success_msg','Categoria editada com sucesso')
+            res.redirect('/admin/categorias',)
+        }).catch((err)=>{
+            req.flash('error_msg','Houve algum erro interno ao salvar a categoria')
+            res.redirect('/admin/categorias')
+        })
+    }).catch((err)=>{
+        req.flash('error_msg','Houve um erro ao editar a categoria')
+        res.redirect('/admin/categorias')
+    })
+})
+router.post('/admin/categorias/deletar',(req,res)=>{
+    Categoria.deleteOne({_id:req.body.id}).then(()=>{
+        req.flash('success_msg','Categoria foi deletada com sucesso')
+        res.redirect('/admin/categorias')
+    }).catch((err)=>{
+        req.flash('error_msg','Houve algum erro ao deletar')
+        res.redirect('/admin/categorias')
+    })
+})
+
+
 
 module.exports = router
 
